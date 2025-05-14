2a193df72f7c018e4361fb04cb259b8a63ac8bf1
Abishek <110195226+Abishhhekkk@users.noreply.github.com>
2025-05-14 14:26:06 +0530

    main

--- script.js
+++ script.js
@@ -103,9 +103,3 @@ window.onload = () => {
         }
     }
 }
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("navbar-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
